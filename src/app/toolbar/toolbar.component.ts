b0ef868f1f75f07c9f096ba7e62eed5b055a2f7a
Robert Israelsson <israelsson.r@gmail.com>
2021-09-01 21:06:10 +0200

    Added toolbar component with save button

---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { faSave } from '@fortawesome/free-solid-svg-icons/faSave';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent {
+
+  @Output() saveEvent = new EventEmitter<string>();
+  faSave = faSave;
+
+  onSave() {
+    console.log("Saving...");
+    this.saveEvent.emit("save");
+  }
+}
