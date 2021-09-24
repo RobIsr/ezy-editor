76a4beb5118123efb678b0bbe9faa60f7e50ad23
Robert Israelsson <israelsson.r@gmail.com>
2021-09-24 11:56:29 +0200

    Added service to handle socket communication.

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { identity } from 'lodash';
 import { Socket } from 'ngx-socket-io';
 import { map } from 'rxjs/operators';
+import { Doc } from './models/doc';
 
 @Injectable()
-export class ChatService {
+export class SocketService {
   constructor(private socket: Socket) {}
 
-  sendMessage(msg: string) {
-    this.socket.emit('message', msg);
+  sendMessage(id: string, name: string, html: string) {
+    var document = {id: id, name: name, html: html}
+    this.socket.emit('message', document);
   }
+
   getMessage() {
     return this.socket.fromEvent('message').pipe(map((data:any) => data.msg));
+  }
+
+  createRoom(docId: string) {
+    this.socket.emit('create', docId);
   }
 }
